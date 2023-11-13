8a7a8ecf6425ddc8af264516f87efc6d383623dc
VlaDi4eKK <v.samolovov@botsbits.io>
2023-11-13 20:24:21 +0500

    initial commit

---
+++ page.tsx
@@ -0,0 +1,25 @@
+import { useCallback } from "react"
+import { useNavigate } from "react-router-dom"
+
+import Button from "../../components/shared/button"
+
+function Module1() {
+  const navigate = useNavigate()
+
+  const handleOpenStartPage = useCallback(() => {
+    navigate("/")
+  }, [navigate])
+
+  return (
+    <div className="flex h-screen flex-col">
+      <div className="m-auto">
+        <h1 className="text-red mx-auto text-3xl font-bold underline">Module 1</h1>
+        <Button onClick={handleOpenStartPage} className="mx-auto">
+          Go to menu
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Module1
