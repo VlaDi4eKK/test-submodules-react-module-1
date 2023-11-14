fbc8623b306d9aaab960b4518c4444b4443ed56b
VlaDi4eKK <v.samolovov@botsbits.io>
2023-11-14 19:14:52 +0500

    add type for button

--- page.tsx
+++ page.tsx
@@ -14,7 +14,7 @@ function Module1() {
     <div className="flex h-screen flex-col">
       <div className="m-auto">
         <h1 className="text-red mx-auto text-3xl font-bold underline">Module 1 from release v1</h1>
-        <Button onClick={handleOpenStartPage} className="mx-auto">
+        <Button type="secondary" onClick={handleOpenStartPage} className="mx-auto">
           Go to menu
         </Button>
       </div>
